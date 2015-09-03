29d422db9f7941e3c3ef9dad61ae2c80dc9b92fb
rafeh01 <rafehqazi1@gmail.com>
2015-09-03 17:26:16 -0500

    added 2048_merge.py in unresolved_problems

---
+++ unresolved_problems/2048_merge.py
@@ -0,0 +1,11 @@
+'''
+Tiles should slide left, equal number pairs should get merged.
+As you work on your merge function, here are some simple tests you should try:
+
+[2, 0, 2, 4] should return [4, 4, 0, 0]
+[0, 0, 2, 2] should return [4, 0, 0, 0]
+[2, 2, 0, 0] should return [4, 0, 0, 0]
+[2, 2, 2, 2, 2] should return [4, 4, 2, 0, 0]
+[8, 16, 16, 8] should return [8, 32, 8, 0]
+
+'''
