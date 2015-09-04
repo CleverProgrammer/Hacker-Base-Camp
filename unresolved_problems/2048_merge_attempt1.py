edddda9369c9abc21df57bef9a6b480522fa6e8e
rafeh01 <rafehqazi1@gmail.com>
2015-09-03 23:14:47 -0500

    2048 updated up to PEP8 Style Analysis Standards and code is improved

--- unresolved_problems/2048_merge_attempt1.py
+++ unresolved_problems/2048_merge_attempt1.py
@@ -1,36 +1,51 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep  3 17:55:56 2015
-2048_merge.py
+2048_merge_attempt1.py
 @author: Rafeh
 """
-nums = [2, 0, 2, 4]
+# import unittest
 
-# Append non-zeroes first
-slide = []
+
+nums = [0, 0, 2, 2]
+
+slide = []  # Append non-zeroes first
 for num in nums:
     if num != 0:
         slide.append(num)
-
-# Append zeroes last
 for num in nums:
     if num == 0:
         slide.append(num)
-        
 pairs = []
 for idx, num in enumerate(slide):
     if idx == len(slide)-1:
+        pairs.append(num)
         break
-    pair = slide[idx] == slide[idx+1]
-    if pair:
-        if idx == 0:
-            pairs.append(slide[idx]*2)
-            pairs.append(0)
-        elif slide[idx] != slide[idx-1]: # Check if it's been already paired
-            pairs.append(slide[idx]*2)
-            pairs.append(0)
+    if num == slide[idx+1]:
+        if num != 0:
+            pairs.append(num*2)
+            slide[idx+1] -= slide[idx+1]
+            pairs.append(slide[idx+1])
+        else:
+            pairs.append(num)
+print(pairs)
 
-print(" Nums: %s" %nums)
-print("Slide: %s" %slide)
-print("Pairs: %s" %pairs)
-        
+
+
+# class Test(unittest.TestCase):
+#    # python -m unittest 2048_merge_attempt1.py in Terminal
+#    def test1(self):
+#        # self.assertEqual(function(input), expected_output)
+#        self.assertEqual(merge([0, 0, 2, 2]), [4, 0, 0, 0])
+#
+#    def test2(self):
+#        self.assertEqual(merge([2, 0, 2, 4]), [4, 4, 0, 0])
+#
+#    def test3(self):
+#        self.assertEqual(merge([2, 2, 0, 0]), [4, 0, 0, 0])
+#
+#    def test4(self):
+#        self.assertEqual(merge([2, 2, 2, 2, 2]), [4, 4, 2, 0, 0])
+#
+#    def test5(self):
+#        self.assertEqual(merge([8, 16, 16, 8]), [8, 32, 8, 0])
