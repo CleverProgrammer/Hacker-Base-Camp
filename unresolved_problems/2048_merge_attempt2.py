7edc293de2f0db69bbff0cb3e00ebf9c302857e0
rafeh01 <rafehqazi1@gmail.com>
2015-09-03 19:42:33 -0500

    comitting unresolved problems

--- unresolved_problems/2048_merge_attempt2.py
+++ unresolved_problems/2048_merge_attempt2.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep  3 17:55:56 2015
-2048_merge.py
+2048_merge_attempt2.py
 @author: Rafeh
 """
 nums = [2, 0, 2, 4]
@@ -21,14 +21,17 @@ pairs = []
 for idx, num in enumerate(slide):
     if idx == len(slide)-1:
         break
-    pair = slide[idx] == slide[idx+1]
-    if pair:
+    if slide[idx] == slide[idx+1]:
+        pairs.append(slide[idx] * 2)
+        idx = idx + 1
+    else:
+        pairs.append(slide[idx])
+        
+    if slide[idx] == slide[idx+1]:  
         if idx == 0:
             pairs.append(slide[idx]*2)
-            pairs.append(0)
         elif slide[idx] != slide[idx-1]: # Check if it's been already paired
             pairs.append(slide[idx]*2)
-            pairs.append(0)
 
 print(" Nums: %s" %nums)
 print("Slide: %s" %slide)
